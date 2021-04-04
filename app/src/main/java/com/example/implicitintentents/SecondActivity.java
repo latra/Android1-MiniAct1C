ddef977ba3a0d99b87ab719f8759052ac903c9e4
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-04-05 01:35:35 +0200

    Second activity.

--- app/src/main/java/com/example/implicitintentents/SecondActivity.java
+++ app/src/main/java/com/example/implicitintentents/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.implicitintentents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        Uri data = intent.getData();
+        TextView urlLabel = findViewById(R.id.urlTextLabel);
+        urlLabel.setText(data.toString());
     }
 }
